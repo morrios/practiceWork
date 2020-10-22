68df79b9222e8df3203517ee6e460e6a19a74ad0
杜林伟 <dulinwei@meidaifu.com>
2020-10-22 12:00:16 +0800

    46.全排列

---
+++ 494.目标和/main.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  494.目标和
+//
+//  Created by 杜林伟 on 2020/10/21.
+//  Copyright © 2020 杜林伟. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int findTargetSumWays(int* nums, int numsSize, int S){
+    int count = 0;
+   
+    return count;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    int nums[5] = {1,1,1,1,1};
+    int res = findTargetSumWays(nums, 5, 3);
+    printf("结果：%d\n", res);
+    
+    return 0;
+}
+/*
+ 0  1 -> 0
+ 2  3 -> 1
+
+ */
