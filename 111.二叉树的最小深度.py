6cce29c51d2ae1589a85afd817450f51c3d0c1ce
morrios <690243233@qq.com>
2020-10-23 15:16:39 +0800

    save

--- 111.二叉树的最小深度.py
+++ 111.二叉树的最小深度.py
@@ -1,7 +1,24 @@
 #-*- coding: UTF-8 -*-
 #!/usr/bin/python3
-from typing import List
+# 给定一个二叉树，找出其最小深度。
 
+# 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+# 说明：叶子节点是指没有子节点的节点。
+
+# 示例 1：
+# 输入：root = [3,9,20,null,null,15,7]
+# 输出：2
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution:
+    # def minDepth(self, root: TreeNode) -> int:
 
 if __name__ == '__main__':
 	print('main')
